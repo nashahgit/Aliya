750f0389ddc288a893d0d4bada1b504b5d84bdce
nashahgit <nashah25@gmail.com>
2016-06-13 11:03:03 +0530

    First file adding

---
+++ test.php
@@ -0,0 +1,287 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"> 
+		
+	   	<title> HOME | Aliya International Academy</title>
+	   	<meta name="description" content="Home Page of Aliya International Academy">
+		<meta name="keywords" content="HTML,CSS,JavaScript">
+		<meta name="author" content="Nasruddin Shah">
+	   	<meta name="viewport" content="width=device-width, initial-scale=1">
+	   	<link rel="icon" href="favicon1.gif" type="image/gif" sizes="20x16">
+	   	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">    
+	   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+	   	<link rel="stylesheet" href="colorbox.css" type="text/css">
+	   	<link rel="stylesheet" type="text/css" href="style.css">
+	   	<link rel="stylesheet" type="text/css" href="homepage.min.css">
+	   	<link rel="stylesheet" type="text/css" href="http://www.jqueryscript.net/demo/Mobile-Friendly-Bootstrap-Tabs-Enhancement-with-jQuery/dist/css/bootstrap-responsive-tabs.css">
+	   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	   	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	   	<script src="responsive-tabs.js"></script>
+
+	   	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js"></script>
+	   	<script src="script.js"></script>
+
+	</head>
+	<body>
+		<div id="wrap">
+			<!-- Begin page content -->
+			<div class="container-fluid">
+				<div class="row">
+		    		<div class="col-sm-12 header-line">
+		    		</div>
+		    	</div>
+			</div>
+		    <div class="container">
+		    	<div class="row">
+		    		<div class="col-sm-6 pull-left">
+		    			<div id="header">
+		    				<img class="header-image  img-responsive" src="aliya_75.png" >
+		    			</div>
+		    		</div>
+		    		<div class="col-sm-6 pull-right" >
+		    			<div id="header-text">
+		    				<img class="img-responsive" src="Aliya_academy_text_logo.png">
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+
+		    <div class="container">
+		    	<!-- Static navbar -->
+		      <nav class="navbar navbar-default">
+		        <div class="container">
+		          <div class="navbar-header">
+		            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		              <span class="sr-only">Toggle navigation</span>
+		              <span class="icon-bar"></span>
+		              <span class="icon-bar"></span>
+		              <span class="icon-bar"></span>
+		            </button>
+		            <!--a class="navbar-brand" href="#">Project name</a-->
+		          </div>
+		          <div id="navbar" class="navbar-collapse collapse">
+		            <ul class="nav navbar-nav">
+		              <li class="active"><a href="#">Home</a></li>
+		              <li><a href="#">About</a></li>
+		              <li><a href="#">Academic</a></li>
+		              <li><a href="#">Alumni</a></li>
+		              <li><a href="#">Gallery</a></li>
+		              <li><a href="#">Contact</a></li>
+		              <li class="dropdown">
+		                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		                <ul class="dropdown-menu">
+		                  <li><a href="#">Action</a></li>
+		                  <li><a href="#">Another action</a></li>
+		                  <li><a href="#">Something else here</a></li>
+		                  <li role="separator" class="divider"></li>
+		                  <li class="dropdown-header">Nav header</li>
+		                  <li><a href="#">Separated link</a></li>
+		                  <li><a href="#">One more separated link</a></li>
+		                </ul>
+		              </li>
+		            </ul>
+		          </div><!--/.nav-collapse -->
+		        </div><!--/.container-fluid -->
+		      </nav>
+		    </div>
+			<div id="feature-banner" class="container carousel slide margin-bottom-40">
+				<ol class="carousel-indicators" aria-label="featured stories">
+					<li role="presentation" class="active">
+						<button data-target="#feature-banner" data-slide-to="0">
+							<span class="screen-reader">Story 1 of 5: Building collaboration</span>
+						</button>
+					</li>
+					<li role="presentation">
+						<button data-target="#feature-banner" data-slide-to="1">
+							<span class="screen-reader">Story 2 of 5: Next generation of literature</span>
+						</button>
+					</li>
+					<li role="presentation">
+						<button data-target="#feature-banner" data-slide-to="2">
+							<span class="screen-reader">Story 3 of 5: Tools of thinking</span>
+						</button>
+					</li>
+					<li role="presentation">
+						<button data-target="#feature-banner" data-slide-to="3">
+							<span class="screen-reader">Story 4 of 5: Taking global action</span>
+						</button>
+					</li>
+					<li role="presentation">
+						<button data-target="#feature-banner" data-slide-to="4">
+							<span class="screen-reader">Story 5 of 5: Technology for good</span>
+						</button>
+					</li>
+				</ol> 
+				<div class="carousel-inner">
+					<div class="item active" tabindex="0">
+						<a href="" class="su-link" data-ua-action="feature1.html" data-ua-label="Building+collaboration" title="Photo Credit: Eric Koziol"><img src="slider_img_1.jpg" alt="Three people in a workspace talking with each other">
+							<div class="carousel-caption  left bg-white">
+								<h3 class="red">Building collaboration</h3>
+								<p class="dark">Stanford's trend of creating buildings that inspire teamwork across disciplines produces innovative discoveries.</p>
+							</div>
+						</a>
+					</div>
+					<div class="item" tabindex="-1">
+						<a href="" class="su-link" data-ua-action="feature2.html" data-ua-label="Next+generation+of+literature" title="Photo Credit: David Gonzlaes"><img src="slider_img_2.jpg" alt="Adam Johnson gesturing in front of a classroom">
+							<div class="carousel-caption  left-top bg-white">
+								<h3 class="red">Next generation of literature</h3>
+								<p class="dark">In Stanford's English Department and Creative Writing Program, students learn from award-winning writers.</p>
+							</div>
+						</a>
+					</div>
+					<div class="item" tabindex="-1">
+					  <a href="" class="su-link" data-ua-action="feature3.html" data-ua-label="Tools+of+thinking" title="Photo Credit: L.A. Cicero"><img src="slider_img_3.jpg" data-src="/rw_includes/homepage/images/2015-12-04_ht_philosophy_0386a.jpg" alt="Philosophy Professors John Perry and Ken Taylor walking in arcade on the Quad">
+					    <div class="carousel-caption  left bg-white">
+					      <h3 class="red">Tools of thinking</h3>
+					        <p class="dark">Stanford's Philosophy Department trains the leaders and thinkers whose great ideas may change the world.</p>
+					    </div>
+					  </a>
+					</div>
+					<div class="item" tabindex="-1">
+					  <a href="" class="su-link" data-ua-action="feature4.html" data-ua-label="Taking+global+action" title="Photo Credit: NASA"><img src="slider_img_4.jpg" data-src="/rw_includes/homepage/images/2015-11-30_ht_copQA_NASA_sunearth_2.jpg" alt="View of Earth from space">
+					    <div class="carousel-caption  left bg-white">
+					      <h3 class="red">Taking global action</h3>
+					        <p class="dark">Stanford researchers are at the forefront of efforts to tackle the challenges caused by climate change.</p>
+					    </div>
+					  </a>
+					</div>
+					<div class="item" tabindex="-1">
+					  <a href="" class="su-link" data-ua-action="feature5.html" data-ua-label="Technology+for+good" title="Photo Credit: L.A. Cicero"><img src="slider_img_5.jpg" data-src="/rw_includes/homepage/images/2015-11-19_ht_CS4good-0181.jpg" alt="CS+Social Good students Nikhita Obeegadoo, Eric Ehizokhale and Sophie Ye working on laptop computers">
+					    <div class="carousel-caption  left bg-dark">
+					      <h3 class="white">Technology for good</h3>
+					        <p class="white">Stanford undergraduates use computer science to make the world a better place.</p>
+					    </div>
+					  </a>
+					</div>
+				</div>
+				<button class="left carousel-control" data-target="#feature-banner" data-slide="prev" accesskey="p"><i class="fa fa-angle-left"></i>
+					<span class="screen-reader">Previous story: Technology for good</span>
+				</button>
+				<button class="right carousel-control" data-target="#feature-banner" data-slide="next" accesskey="n"><i class="fa fa-angle-right"></i>
+					<span class="screen-reader">Next story: Next generation of literature</span>
+				</button>
+			</div>
+
+			<div class="container margin-bottom-20">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="headline"><h3>ABOUT US</h3></div>
+					<!--Card-->
+						<div class="card">
+						    <!--Card image-->
+						    <img class="img-fluid" src="http://mdbootstrap.com/images/regular/nature/img%20(28).jpg" alt="Card image cap">
+						    <!--/.Card image-->
+						    <!--Card content-->
+						    <div class="card-block">
+						        <!--Title-->
+						        <h4 class="card-title">Card title</h4>
+						        <!--Text-->
+						        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						        <a href="#" class="btn btn-primary">Button</a>
+						    </div>
+						    <!--/.Card content-->
+						</div>
+						<!--/.Card-->
+					</div>
+					<div class="col-sm-9" id="news">
+						<div class="headline"><h3>NEWS & EVENTS</h3></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="card">
+									<img class="img-fluid" src="http://mdbootstrap.com/images/regular/nature/img%20(18).jpg" alt="Card image cap">
+								    <!--/.Card image-->
+								    <!--Card content-->
+								    <div class="card-block">
+								        <!--Title-->
+								        <h4 class="card-title">Card title</h4>
+								        <!--Text-->
+								        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								        <a href="#" class="btn btn-primary">Button</a>
+								    </div>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="card">
+									<img class="img-fluid" src="http://mdbootstrap.com/images/reg/reg%20(62).jpg" alt="Card image cap">
+								    <!--/.Card image-->
+								    <!--Card content-->
+								    <div class="card-block">
+								        <!--Title-->
+								        <h4 class="card-title">Card title</h4>
+								        <!--Text-->
+								        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								        <a href="#" class="btn btn-primary">Button</a>
+								    </div>
+								</div>
+							</div>
+						</div>	
+					</div>	
+				</div>
+			</div>
+
+			<div class="container margin-bottom-20">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="headline"><h3>MESSAGES</h3></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="container margin-bottom-20">
+				<div class="headline"><h3>PROGRAMS</h3></div>
+				<div class="row">
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="24X7.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="Library.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="LangLab.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="AcademicBank.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="IT.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="Perfoming.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="LogoKuthba.png" alt="Card image cap">
+						</div>
+					</div>
+					<div class="col-sm-3 col-xs-6">
+						<div class="card">
+							<img class="img-fluid" src="Studio.png" alt="Card image cap">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		    <div id="push"></div>
+		</div>
+
+	    <div id="footer">
+	      	<div class="container">
+	        	
+	      	</div>
+	    </div>
+	</body>
+</html>
